78b1c1179240ac1f7e28ba07fed2c88ef9bf2f67
Mallory Porche <malloryburke@gmail.com>
2014-11-22 20:14:29 -0800

    Button placement complete

--- Tuts & Projects/MobileMenu/js/app.js
+++ Tuts & Projects/MobileMenu/js/app.js
@@ -7,13 +7,10 @@ $("#menu a").each(function () {
 	var $anchor = $(this);
 	//Create an option
 	var $option = $("<option></option>");
-
 	//Option's value is the href 
 	$option.val($anchor.attr("href"));
 	//Option's text is the text of links
-
 	$option.text($anchor.text());
-
 	//Append option select
 	$select.append($option);
 })
@@ -21,8 +18,14 @@ $("#menu a").each(function () {
 	
 	
 //Create a button
+var $button = $("<button>Go</button>");
+$("#menu").append($button)
 	//Bind click to button to 
+	$button.click(function() {
 	//go to Select location
+	window.location = $select.val();	
+	})
+	
 //Modify CSS to hide links on small widthds and show button and select
 //Also hides select and button on larger widthds and shows links
 //Deal with selected options
