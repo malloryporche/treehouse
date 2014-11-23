db44d4560ecc0c723a82a09dd31e107f5fc9accd
Mallory Porche <malloryburke@gmail.com>
2014-11-22 17:41:26 -0800

    Create a select and append to #menu

---
+++ Tuts & Projects/MobileMenu/js/app.js
@@ -0,0 +1,14 @@
+//Create a select and append to menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+//Cycle over menu links
+	//Creat an options
+	//Option's value is the href 
+	//Option's text is the text of links
+	//Append option select
+//Create a button
+	//Bind click to button to 
+	//go to Select location
+//Modify CSS to hide links on small widthds and show button and select
+//Also hides select and button on larger widthds and shows links
+//Deal with selected options
